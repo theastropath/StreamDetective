a39a1d3d7e450988dafb8130b874ead4e7d528b2
Die4Ever <die4ever2005@gmail.com>
2022-12-05 01:03:21 -0600

    more fixes for autoinstaller

--- autoinstaller/__init__.py
+++ autoinstaller/__init__.py
@@ -20,14 +20,18 @@ def check_requirement(r):
         install(r)
         # make sure it installed properly, this especially helps catch missing entries in import_names
         importlib.invalidate_caches()
-        importlib.import_module(m)
-        #if not importlib.util.find_spec(m):
-        #    raise Exception('failed to install '+r)
+        import pip
+        if not pip.locations.user_site in sys.path:
+            sys.path.append(pip.locations.user_site)
+        #importlib.import_module(m)
+        if not importlib.util.find_spec(m):
+            raise Exception('failed to install '+r)
 
 
 def install(r):
     print('need to install:', r)
-    subprocess.run(["pip3", "install", r])
+    subprocess.run(["pip3", "install", r], check=True, capture_output=True)
+    #pip.main(['install', '--user', r])
 
 
 with open('requirements.txt') as requirements:
